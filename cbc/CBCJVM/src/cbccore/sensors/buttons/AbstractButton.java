c3717880f91b508cd011cffbcbc59ef3f745d25c
Braden McDorman <bmcdorman@gmail.com>
2010-01-15 19:12:36 -0600

    Added isPushed and isNotPushed to buttons

--- cbc/CBCJVM/src/cbccore/sensors/buttons/AbstractButton.java
+++ cbc/CBCJVM/src/cbccore/sensors/buttons/AbstractButton.java
@@ -3,5 +3,10 @@ package cbccore.sensors.buttons;
 import cbccore.sensors.IBooleanSensor;
 
 public abstract class AbstractButton implements IBooleanSensor {
-
+	public boolean isPushed() {
+		return getValue();
+	}
+	public boolean isNotPushed() {
+		return !getValue();
+	}
 }

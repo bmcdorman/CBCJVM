70ebdc141d9a5d731c7018a19d447a93406f7a83
Braden McDorman <bmcdorman@gmail.com>
2010-01-16 17:04:31 -0600

    Added tests.. hopefully

---
+++ cbc/CBCJVM/tests/Events/bin/Main.java
@@ -0,0 +1,50 @@
+//package cbccore.tests;
+
+import cbccore.Device;
+import cbccore.events.Event;
+import cbccore.events.EventDispatcher;
+import cbccore.events.EventEmitter;
+import cbccore.events.EventListenerAdapter;
+import cbccore.events.IEventListener;
+
+public class Main {
+	// Dummy event
+	public final class DriveBegan extends Event {}
+	public final class DestinationReached extends Event {}
+	// Dummy emitter
+	public class Driver extends EventEmitter {
+		public void drive() {
+			emit(new DriveBegan());
+			// ...
+			emit(new DestinationReached());
+		}
+	}
+	
+	// Beeps
+	public class Beeper implements IEventListener {
+		@Override
+		public void eventDispatched(EventEmitter emitter, Event type) {
+			System.out.println("BEEP!");
+		}
+	}
+	
+	public Main() {
+		EventDispatcher dispatcher = EventDispatcher.getInstance();
+		Driver driver = new Driver();
+		dispatcher.addEventListener(driver, new DestinationReached(), new Beeper());
+		
+		// Inline class ftw!
+		dispatcher.addEventListener(driver, new DriveBegan(), new EventListenerAdapter() {
+			@Override
+			public void eventDispatched(EventEmitter emitter, Event type) {
+				System.out.println("DRIVE BEGAN!");
+			}
+		});
+		driver.drive();
+	}
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}

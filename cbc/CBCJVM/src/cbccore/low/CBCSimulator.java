b43e8390c423261dc88a8262be4007dc88cf2def
PiPeep <pipeep@pipeep-laptop.(none)>
2010-02-12 18:00:27 -0500

    Cleaned up a few things, some reorganization of simulator, fixed button pressing for simulator, so now you only need to click once.

--- cbc/CBCJVM/src/cbccore/low/CBCSimulator.java
+++ cbc/CBCJVM/src/cbccore/low/CBCSimulator.java
@@ -28,12 +28,17 @@ import java.awt.Container;
 import java.awt.ComponentOrientation;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import java.io.PrintStream;
 
 /**
+ * Do not instanciate this class. It will automatically be created the first
+ * time a CBCJVM API method and/or class is used. It is handled by
+ * cbccore.Device
  * 
- * @author Benjamin Woodruff
- *
+ * @author Benjamin Woodruff, Braden McDorman
  */
 
 public class CBCSimulator {
@@ -48,7 +53,10 @@ public class CBCSimulator {
 	public SimulatedCamera camera;
 	public SimulatedCreate create;
 	
+	
+	//sidebar things
 	public MotorSpeed[] m;
+	Container sidebar = new Container();
 	
 	public static PrintStream stdOut;
 	
@@ -57,11 +65,15 @@ public class CBCSimulator {
 	private boolean framebuffersAdded = false;
 	
 	public CBCSimulator() {
+		
+		//Blend in with Native look
 		try {
-		      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		    } catch(Exception e) {
-		      System.out.println("Error setting native LAF: " + e);
-		    }
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch(Exception e) {
+			System.out.println("Error setting native LAF: " + e);
+		}
+		
+		//create simulated devices, to replace CBC methods
 		sound = new SimulatedSound(this);
 		sensor = new SimulatedSensor(this);
 		device = new SimulatedDevice(this);
@@ -71,23 +83,41 @@ public class CBCSimulator {
 		motor = new SimulatedMotor(this);
 		camera = new SimulatedCamera(this);
 		create = new SimulatedCreate(this);
-		
 		m = new MotorSpeed[4];
 		for(int i = 0; i < m.length; ++i) {
 			m[i] = new MotorSpeed(0, false);
 		}
 		
+		//stdOut: redirect System.out.println to the builtin console
 		if(stdOut == null) { stdOut = System.out; }
 		System.out.println("Welcome to CBCJava");
-		
-		
 		frame.getContentPane().add(new JScrollPane(display.getTextBox()), BorderLayout.CENTER);
+		System.setOut(display.getPrintStream());
 		
+		//Create sidebar
+		addSidebar();
+		
+		//buttons
+		addButtons();
+		
+		//Setup visibility settings for root pane
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		//Make sure the window isn't too big for the person's screen
+		frame.setSize(Math.min(1024, (int)(screenSize.width*.7)), Math.min(768, (int)(screenSize.height*.7)));
+		frame.setVisible(true);
+	}
+	
+	private void addSidebar() {
+		GridLayout sidebarLayout = new GridLayout(2, 1, 0, 5);
+		sidebar.setLayout(sidebarLayout);
+		frame.getContentPane().add(sidebar, BorderLayout.EAST);
+	}
+	
+	private void addButtons() {
 		FlowLayout buttonLayout = new FlowLayout();
 		Container buttonContainer = new Container();
 		buttonContainer.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		//buttonContainer.add(input.ub, BorderLayout.SOUTH);
-		//buttonContainer.add(input.db, BorderLayout.SOUTH);
 		buttonContainer.setLayout(buttonLayout);
 		buttonContainer.add(input.ub);
 		buttonContainer.add(input.db);
@@ -97,23 +127,15 @@ public class CBCSimulator {
 		buttonContainer.add(input.bb);
 		buttonContainer.add(input.blb);
 		frame.getContentPane().add(buttonContainer, BorderLayout.SOUTH);
-		
-		System.setOut(display.getPrintStream());
-		
-		frame.setSize(320, 240);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setSize(1024, 768);
 	}
 	
+	/**
+	 * Checks to see if the framebuffer has already been added. If not, it adds
+	 * it.
+	 */
 	public void addFramebuffers() {
 		if(framebuffersAdded) return;
 		framebuffersAdded = true;
-		Container framebuffers = new Container();
-		GridLayout framebufferLayout = new GridLayout(1, 1);
-		framebuffers.setLayout(framebufferLayout);
-		framebuffers.add(((SimulatedFramebuffer)cbccore.display.Display.getFramebuffer()).getPanel());
-		frame.getContentPane().add(framebuffers, BorderLayout.EAST);
-		
+		sidebar.add(((SimulatedFramebuffer)cbccore.display.Display.getFramebuffer()).getPanel(), 0);
 	}
 }

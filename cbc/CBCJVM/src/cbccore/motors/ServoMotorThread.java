8cb5cd41c5b4beb083b6bb6c55071d852ff4ca6d
Braden McDorman <bmcdorman@gmail.com>
2010-04-24 10:47:40 -0500

    Added ServoMotorThread, which runs all ServoMotors

---
+++ cbc/CBCJVM/src/cbccore/motors/ServoMotorThread.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class ServoMotorThread extends Thread {
+	public static ServoMotorThread get() {
+		if (instance == null) {
+			instance = new ServoMotorThread();
+			instance.start();
+		}
+		return instance;
+	}
+	private ArrayList<NewServoMotor> servoMotors = new ArrayList<NewServoMotor>();
+
+	private boolean exit = false;
+
+	private static ServoMotorThread instance = null;
+
+	public ServoMotorThread() {
+		setDaemon(true);
+	}
+
+	public void addServoMotor(NewServoMotor servoMotor) {
+		synchronized (servoMotors) {
+			servoMotors.add(servoMotor);
+		}
+	}
+
+	public void exit() {
+		exit = true;
+	}
+
+	@Override
+	public void run() {
+		ArrayList<NewServoMotor> removes = new ArrayList<NewServoMotor>();
+		while (!exit) {
+			removes.clear();
+			synchronized (servoMotors) {
+				for (NewServoMotor servoMotor : servoMotors) {
+					servoMotor.update();
+					if (!servoMotor.isMoving()) {
+						removes.add(servoMotor);
+					}
+				}
+				servoMotors.removeAll(removes);
+			}
+		}
+	}
+}
